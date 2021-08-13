a6583a4ba283b7123f071ed38dfba9bd68eb6d7b
Rona ¯\_(ツ)_/¯ <ronauli.sidabukke@btpn.com>
2021-08-13 12:21:36 +0700

    feat(worker): add try-catch  in handler

--- src/handlers/sendNotificationHandler.js
+++ src/handlers/sendNotificationHandler.js
@@ -7,22 +7,26 @@ const sendNotificationHandler = async (ctx, parsedMessage) => {
     connectors: { customerConnector, notificationConnector, callbackConnector },
   } = ctx;
 
-  if (!is_testing) {
-    const customer = await customerConnector.getCustomer(customer_id);
-    Object.assign(payload, {
-      callback_url: customer.callback_url,
-      authentication_key: customer.authentication_key,
+  try {
+    if (!is_testing) {
+      const customer = await customerConnector.getCustomer(customer_id);
+      Object.assign(payload, {
+        callback_url: customer.callback_url,
+        authentication_key: customer.authentication_key,
+      });
+    }
+
+    logger.info(`Calling callback of details: ${notification_id}`);
+    await callbackConnector.request({
+      ...payload,
+      customer_id,
     });
+
+    logger.info(`Success trigger, marking notification as complete: ${notification_id}`);
+    await notificationConnector.markAsComplete(notification_id);
+  } catch (e) {
+    logger.error('Error occured during notification handling, skipping message...');
   }
-
-  logger.info(`Calling callback of details: ${notification_id}`);
-  await callbackConnector.request({
-    ...payload,
-    customer_id,
-  });
-
-  logger.info(`Success trigger, marking notification as complete: ${notification_id}`);
-  await notificationConnector.markAsComplete(notification_id);
 };
 
 module.exports = sendNotificationHandler;
